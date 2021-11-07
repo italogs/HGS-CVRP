83afadd6a6f52152507377d09d8892a34ab77105
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2021-11-07 19:38:08 -0300

    updating scripts;

--- Program/all-WK_test-CC-script2.sh
+++ Program/all-WK_test-CC-script2.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --account=def-vidalthi
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=3
 #SBATCH --mem=20G
 
+seed=1
+time=5
 for (( it = ${instance_id} ; it < ${instance_id} + 100; it++))
 do
-    seed=1
-    time=5
+    
     
     useDPDP=0
     crossover=1
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
 
     useDPDP=1
     crossover=1
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
 
-    if ((it % 2 == 1));
-    then
-        
-        wait
-    fi
 
+    useDPDP=0
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
+    wait
+
+    useDPDP=1
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
+
+    useDPDP=0
+    crossover=10
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
+    useDPDP=1
+    crossover=10
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
+    wait
 done
 
 wait
--- Program/all-uchoa-CC-script2.sh
+++ Program/all-uchoa-CC-script2.sh
@@ -2,22 +2,64 @@
 #SBATCH --time=00:01:00
 #SBATCH --account=def-vidalthi
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
 
 
+seed=1
 useDPDP=0
 crossover=1
-for (( seed=1; seed <= 1; seed++));
-do
-    ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
-done
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+
 
 useDPDP=1
 crossover=1
-for (( seed=1; seed <= 1; seed++));
-do
-    ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
-done
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    
+wait
+
+useDPDP=0
+crossover=9
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    
+
+
+useDPDP=1
+crossover=9
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    
+
+
+wait
+
+useDPDP=0
+crossover=10
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    
+
+
+useDPDP=1
+crossover=10
+mkdir -p Solutions/useDPDP${useDPDP}_crossover${crossover}/
+mkdir -p outputs/useDPDP${useDPDP}_crossover${crossover}/
+
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.txt &
+    
+
 
 wait
