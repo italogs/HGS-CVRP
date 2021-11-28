84936812ed78e1b08544d94944c912fa03e867fc
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2021-11-28 14:53:12 -0300

    updating cc scripts for all nbgranular parameters

--- Program/all-WK_test-CC-script2.sh
+++ Program/all-WK_test-CC-script2.sh
@@ -1,33 +1,127 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+#SBATCH --time=01:00:00
 #SBATCH --account=def-vidalthi
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=5
 #SBATCH --mem=20G
 
 seed=1
-time=5
+
+nbGranular=5
 for (( it = ${instance_id} ; it < ${instance_id} + 100; it++))
 do
-    
     useDPDP=0
     crossover=1
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
     useDPDP=1
     crossover=1
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 
     useDPDP=0
     crossover=9
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
     useDPDP=1
     crossover=9
-    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}.txt &
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
     wait
 done
+
+nbGranular=10
+for (( it = ${instance_id} ; it < ${instance_id} + ${nbIt}; it++))
+do
+    useDPDP=0
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+
+    useDPDP=0
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    wait
+done
+
+nbGranular=20
+for (( it = ${instance_id} ; it < ${instance_id} + ${nbIt}; it++))
+do
+    useDPDP=0
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+
+    useDPDP=0
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    wait
+done
+
+
+nbGranular=30
+for (( it = ${instance_id} ; it < ${instance_id} + ${nbIt}; it++))
+do
+    useDPDP=0
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+
+    useDPDP=0
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    wait
+done
+
+nbGranular=50
+for (( it = ${instance_id} ; it < ${instance_id} + ${nbIt}; it++))
+do
+    useDPDP=0
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=1
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+
+    useDPDP=0
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    useDPDP=1
+    crossover=9
+    ./genvrp ../Instances/CVRP/WK_test_${it}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/WK_test_${it}.vrp_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_WK_test_${it}.vrp_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+    wait
+done
+
 
 wait
--- Program/all-uchoa-CC-script2.sh
+++ Program/all-uchoa-CC-script2.sh
@@ -2,83 +2,110 @@
 #SBATCH --time=00:01:00
 #SBATCH --account=def-vidalthi
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=5
 #SBATCH --mem=20G
-
-
 seed=1
+
+
+nbGranular=5
+
 useDPDP=0
 crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+useDPDP=1
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+useDPDP=0
+crossover=9
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+    
+useDPDP=1
+crossover=9
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+wait
+
+nbGranular=10
+
+useDPDP=0
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+useDPDP=1
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+useDPDP=0
+crossover=9
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+    
+useDPDP=1
+crossover=9
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
+
+wait
+
+
 nbGranular=20
-./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
+useDPDP=0
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 useDPDP=1
 crossover=1
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
-    
-wait
 
 useDPDP=0
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
     
 useDPDP=1
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 wait
 
 
-
-useDPDP=0
-crossover=1
 nbGranular=30
-./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
+useDPDP=0
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 useDPDP=1
 crossover=1
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
-    
-wait
 
 useDPDP=0
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
     
 useDPDP=1
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 wait
 
 
-
-useDPDP=0
-crossover=1
 nbGranular=50
-./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
+useDPDP=0
+crossover=1
+./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 useDPDP=1
 crossover=1
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
-    
-wait
 
 useDPDP=0
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
     
 useDPDP=1
 crossover=9
-
 ./genvrp ../Instances/CVRP/${instance}.vrp Solutions/useDPDP${useDPDP}_crossover${crossover}/${instance}_crossover${crossover}_useDPDP${useDPDP}_time${time}_seed${seed}_nbGranular${nbGranular}.sol -crossover ${crossover} -useDPDP ${useDPDP} -t ${time} -seed ${seed} -nbGranular ${nbGranular} &> outputs/useDPDP${useDPDP}_crossover${crossover}/output_${instance}_useDPDP${useDPDP}_crossover${crossover}_time${time}_seed${seed}_nbGranular${nbGranular}.txt &
 
 wait
