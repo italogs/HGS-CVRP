24b3e7b8525f4b2b2de69fd162bd10bedb39e51f
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2021-12-23 05:08:42 -0300

    removed unused scripts; updated script

--- Program/all-WK_test-VRPSolver-CC-1step.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir CVRP/Solutions
-
-for (( instance_id=0; instance_id <= 9999; ));
-do
-	sbatch --export=instance_id=${instance_id} $HOME/script-for-VRPSolver-CC-2step.sh
-	instance_id=$(($instance_id + 5))
-done
