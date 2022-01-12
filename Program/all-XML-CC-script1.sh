948cec04db4f02cf61aa3c5156bb4ef9182e01e9
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2022-01-12 16:16:28 -0300

    update on XML scripts

--- Program/all-XML-CC-script1.sh
+++ Program/all-XML-CC-script1.sh
@@ -3,7 +3,7 @@
 # Creating folders
 mkdir -p DPDP/Heatmaps_for_HGS/
 
-trained_models=(10dim-5layers-1500epochs 10dim-5layers-500epochs Kool-300dim-30layers-1500epochs) 
+trained_models=(10dim-5layers-1500epochs) 
 for model in "${trained_models[@]}"
 do
     mkdir -p Solutions/${model}/useHeatmap0_crossover1/
@@ -24,8 +24,8 @@ done
 step_instances=500
 time=5
 
-for (( instance_id=0; instance_id <= 9999; ));
+for (( offset=0; offset <= 9999; ));
 do
-    sbatch --export=instance_id=${instance_id},time=${time},step_instances=${step_instances} all-WK_test-CC-script2.sh
-    instance_id=$(($instance_id + $step_instances))
+    sbatch --export=offset=${offset},time=${time},step_instances=${step_instances} all-WK_test-CC-script2.sh
+    offset=$(($offset + $step_instances))
 done
